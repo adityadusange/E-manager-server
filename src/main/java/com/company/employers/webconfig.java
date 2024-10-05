83c296a1559f0eaa2d88c34298ff57acd84521bc
adityadusange <103125893+adityadusange@users.noreply.github.com>
2024-10-05 15:15:58 +0530

    Update webconfig.java

--- src/main/java/com/company/employers/webconfig.java
+++ src/main/java/com/company/employers/webconfig.java
@@ -1,19 +1,15 @@
-package com.company.employers;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class webconfig implements WebMvcConfigurer {
-
+public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://e-manager-view.vercel.app")// Add your frontend URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        registry.addMapping("/**") // Allow all endpoints
+                .allowedOrigins("https://e-manager-view.vercel.app") // Allow this origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
+                .allowCredentials(true); // Allow credentials if necessary
     }
 }
